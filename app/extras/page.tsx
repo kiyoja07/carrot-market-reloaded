7ba59d0ddf04073aabdb2c2fb49ac085347a8283
Yongjae Kim <kiyoja07@gmail.com>
2024-10-30 13:55:24 +0900

    #17.1 Fonts

---
+++ app/extras/page.tsx
@@ -0,0 +1,10 @@
+export default function Extras() {
+  return (
+    <div className="flex flex-col gap-3 py-10">
+      <h1 className="text-6xl font-metallica">Extras!</h1>
+      <h2 className="font-roboto">So much more to learn!</h2>
+      <h2 className="font-rubick">So much more to learn!</h2>
+      <h2>So much more to learn!</h2>
+    </div>
+  );
+}
